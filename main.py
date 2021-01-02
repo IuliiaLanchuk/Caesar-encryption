3e36b91c8e82deb0e8e8632d8fa12262a2bab5b3
IuliiaLanchuk <lanchuk.iuliia@gmail.com>
2021-01-02 15:15:13 +0300

    version 2 with all positive and negative tests

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ digits = '0123456789'
 punctuation = ',!#$%&*+-=?@^_. '
 
 
-def shift_is_valid(shift, language):
+def shift_is_valid(shift, langua):
     if shift.isdigit():
-        if language == 'russian' and 1 <= int(shift) <= 32:
+        if langua == 'russian' and 1 <= int(shift) <= 32:
             lbl_incorrect_format.configure(pady='-50', fg='LightSteelBlue3')
             return True
-        elif language == 'english' and 1 <= int(shift) <= 26:
+        elif langua == 'english' and 1 <= int(shift) <= 26:
             lbl_incorrect_format.configure(pady='-50', fg='LightSteelBlue3')
             return True
         else:
@@ -25,12 +25,12 @@ def shift_is_valid(shift, language):
         return False
 
 
-def language_is_valid(language, text):
-    if language == 'russian' and len(text) != 0:
+def language_is_valid(langu, text):
+    if langu == 'russian' and len(text) != 0:
         for l in range(len(text)):
             if text[l] in russian_alphabet or text[l] in russian_alphabet_upper:
                 return True
-    elif language == 'english' and len(text) != 0:
+    elif langu == 'english' and len(text) != 0:
         for l in range(len(text)):
             if text[l] in english_alphabet or text[l] in english_alphabet_upper:
                 return True
@@ -42,18 +42,20 @@ def validation(proc, lang, shift, text):
     counter = 0
     if proc == 0:
         counter += 1
-        lbl_processNotChosen= Label(window, text='Should not be empty', fg='red', width='17', bg='LightSteelBlue3')
+        lbl_processNotChosen = Label(window, text='Should not be empty', fg='red', width='17', bg='LightSteelBlue3')
         lbl_processNotChosen.place(x=105, y=40)
     else:
-        lbl_processNotChosen = Label(window, text='Should not be empty', fg='LightSteelBlue3', width='17', bg='LightSteelBlue3')
+        lbl_processNotChosen = Label(window, text='Should not be empty', fg='LightSteelBlue3', width='17',
+                                     bg='LightSteelBlue3')
         lbl_processNotChosen.place(x=105, y=40)
 
     if lang == 0:
         counter += 1
-        lbl_langNotChosen= Label(window, text='Should not be empty', fg='red', width='17', bg='LightSteelBlue3')
+        lbl_langNotChosen = Label(window, text='Should not be empty', fg='red', width='17', bg='LightSteelBlue3')
         lbl_langNotChosen.place(x=115, y=93)
     else:
-        lbl_langNotChosen = Label(window, text='Should not be empty', fg='LightSteelBlue3', width='17', bg='LightSteelBlue3')
+        lbl_langNotChosen = Label(window, text='Should not be empty', fg='LightSteelBlue3', width='17',
+                                  bg='LightSteelBlue3')
         lbl_langNotChosen.place(x=115, y=93)
 
     if len(shift) == 0:
@@ -67,12 +69,11 @@ def validation(proc, lang, shift, text):
     if len(text) == 0:
         counter += 1
         lbl_incorrect_language.configure(text='Should not be empty', fg='red')
-        lbl_incorrect_language.place(x=337, y=222)
+        lbl_incorrect_language.place(x=350, y=227)
     else:
         lbl_incorrect_language.configure(text='Should not be empty', fg='LightSteelBlue3', width='17')
-        lbl_incorrect_language.place(x=337, y=222)
+        lbl_incorrect_language.place(x=350, y=227)
     return counter == 0
-
 
 
 # после нажатия на кнопку Generate
@@ -81,7 +82,7 @@ def generate():
     lang = language_elected.get()
     shift = shift_number.get()
     text = phrase.get()
-    
+
     if validation(proc, lang, shift, text):
         if proc == 1:
             process = 'encryption'
@@ -97,7 +98,7 @@ def generate():
         if size != 0:
             result.delete(0, size)
 
-    # проверка на то, что число сдвига ограничено и для каждого языка свое ограничение
+        # проверка на то, что число сдвига ограничено и для каждого языка свое ограничение
         if shift_is_valid(shift, language):
             lbl_incorrect_format.configure(text='Incorrect value', width='14', fg='LightSteelBlue3',
                                            bg='LightSteelBlue3')
@@ -105,43 +106,40 @@ def generate():
             lbl_incorrect_format.configure(text='Incorrect value', width='20', fg='red')
             lbl_incorrect_format.grid(column=1, row=7, sticky='nw')
 
-    # проверка на то, что выбранный язык и запись для шифрования/дешифрования одного языка
+        # проверка на то, что выбранный язык и запись для шифрования/дешифрования одного языка
         if language_is_valid(language, text):
             lbl_incorrect_language.configure(text='Some letters are not in a language you chosen', fg='LightSteelBlue3')
+            for i in range(len(text)):
+                if text[i] in digits or text[i] in punctuation:
+                    shifted.append(text[i])
+                else:
+                    if language == "russian":
+                        if process == 'encryption':
+                            if text[i] in russian_alphabet:
+                                shifted.append(russian_alphabet[(russian_alphabet.find(text[i])) + int(shift)])
+                            else:
+                                shifted.append(russian_alphabet_upper[(russian_alphabet_upper.find(text[i])) + int(shift)])
+                        if process == 'decryption':
+                            if text[i] in russian_alphabet:
+                                shifted.append(russian_alphabet[(russian_alphabet.find(text[i])) - int(shift)])
+                            else:
+                                shifted.append(russian_alphabet_upper[(russian_alphabet_upper.find(text[i])) - int(shift)])
+                    if language == "english":
+                        if process == 'encryption':
+                            if text[i] in english_alphabet:
+                                shifted.append(english_alphabet[(english_alphabet.find(text[i])) + int(shift)])
+                            else:
+                                shifted.append(english_alphabet_upper[(english_alphabet_upper.find(text[i])) + int(shift)])
+                        if process == 'decryption':
+                            if text[i] in english_alphabet:
+                                shifted.append(english_alphabet[(english_alphabet.find(text[i])) - int(shift)])
+                            else:
+                                shifted.append(english_alphabet_upper[(english_alphabet_upper.find(text[i])) - int(shift)])
+            result.insert(END, ''.join(shifted))
+            shifted.clear()
         else:
-            lbl_incorrect_language.configure(text='Some letters are not in a language you chosen', fg='red')
-            lbl_incorrect_language.place(x=337, y=222)
-
-        for i in range(len(text)):
-            if text[i] in digits or text[i] in punctuation:
-                shifted.append(text[i])
-            else:
-                if language == "russian":
-                    if process == 'encryption':
-                        if text[i] in russian_alphabet:
-                            shifted.append(russian_alphabet[(russian_alphabet.find(text[i])) + int(shift)])
-                        else:
-                            shifted.append(russian_alphabet_upper[(russian_alphabet_upper.find(text[i])) + int(shift)])
-                    if process == 'decryption':
-                        if text[i] in russian_alphabet:
-                            shifted.append(russian_alphabet[(russian_alphabet.find(text[i])) - int(shift)])
-                        else:
-                            shifted.append(russian_alphabet_upper[(russian_alphabet_upper.find(text[i])) - int(shift)])
-                if language == "english":
-                    if process == 'encryption':
-                        if text[i] in english_alphabet:
-                            shifted.append(english_alphabet[(english_alphabet.find(text[i])) + int(shift)])
-                        else:
-                            shifted.append(english_alphabet_upper[(english_alphabet_upper.find(text[i])) + int(shift)])
-                    if process == 'decryption':
-                        if text[i] in english_alphabet:
-                            shifted.append(english_alphabet[(english_alphabet.find(text[i])) - int(shift)])
-                        else:
-                            shifted.append(english_alphabet_upper[(english_alphabet_upper.find(text[i])) - int(shift)])
-        result.insert(END, ''.join(shifted))
-        shifted.clear()
-
-
+            lbl_incorrect_language.configure(text='Some letters are not in a language you chosen', fg='red', width='35')
+            lbl_incorrect_language.place(x=350, y=227)
 
 window = Tk()
 window.title("Приложение Шифр Цезаря")
@@ -158,7 +156,7 @@ processes = [("encryption", 1), ("decryption", 2)]
 process_elected = IntVar()
 column = 0
 for txt, val in processes:
-    Radiobutton(text=txt, value=val, variable=process_elected, padx=5, pady=5, bg='LightSteelBlue3').\
+    Radiobutton(text=txt, value=val, variable=process_elected, padx=5, pady=5, bg='LightSteelBlue3'). \
         grid(column=column, row=2, sticky=W)
     column += 1
 
@@ -169,7 +167,7 @@ languages = [("russian", 1), ("english", 2)]
 language_elected = IntVar()
 column = 0
 for txt, val in languages:
-    Radiobutton(text=txt, value=val, variable=language_elected, padx=5, pady=5, bg='LightSteelBlue3').\
+    Radiobutton(text=txt, value=val, variable=language_elected, padx=5, pady=5, bg='LightSteelBlue3'). \
         grid(column=column, row=4, sticky=W)
     column += 1
 
@@ -190,15 +188,13 @@ phrase.grid(column=1, row=8, sticky=W, pady=5)
 
 # кнопка Выполнить
 btn_perform = Button(window, text="Generate", bg="gray", fg="black", command=generate)
-btn_perform.place(x=442, y=240)
+btn_perform.place(x=442, y=250)
 
 lbl_incorrect_format = Label(window, text='Incorrect value', width='14', bg='LightSteelBlue3')
 lbl_result = Label(window, text='Result', font=("Arial", 10, "bold"), width='14', fg='black')
 lbl_result.grid(column=0, row=10, sticky=W, pady=5, padx=2)
-result = Listbox(window,  width='45', height='1')
+result = Listbox(window, width='45', height='1')
 result.place(x=3, y=270)
-lbl_incorrect_language = Label(window, width='40', bg='LightSteelBlue3')
-
-
+lbl_incorrect_language = Label(window, width='19', bg='LightSteelBlue3')
 
 window.mainloop()
